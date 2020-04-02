1fe17aca34bc25d95f259b8399f3ae10333df5bd
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-04-02 11:15:06 +0530

    Move interaction of View and Desk inside Alley.

--- code/drive.java
+++ code/drive.java
@@ -3,15 +3,9 @@ final class drive {
     }
 
     public static void main(final String[] args) {
-
         final int numLanes = 3;
         final int maxPatronsPerParty = 6;
 
-        final Alley a = new Alley(numLanes);
-        final ControlDesk controlDesk = a.getControlDesk();
-
-        final Observer cdv = new ControlDeskView(controlDesk, maxPatronsPerParty);
-        controlDesk.subscribe(cdv);
-
+        new Alley(numLanes, maxPatronsPerParty);
     }
 }
